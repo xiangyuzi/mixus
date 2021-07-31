9fbcebfdf0d2e9cc475f1c6fe04dc50ab7b27a49
xiangyuzi <ayuko.weiji@gmail.com>
2021-07-31 13:01:24 +0900

    userモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :birth_date, presence: true
+  validates :prefecture_id, presence: true
 end
