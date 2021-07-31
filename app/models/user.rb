ed65483ed9a1d0192cfd2d4b7c515d2a57a1b942
xiangyuzi <ayuko.weiji@gmail.com>
2021-07-31 22:56:11 +0900

    ユーザー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,4 +8,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :birth_date, presence: true
   validates :prefecture_id, presence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+
+  belongs_to :Prefecture
 end
