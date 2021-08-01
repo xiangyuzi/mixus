5f01205d7ae362b76b075b82a6609b7baf5d3469
xiangyuzi <ayuko.weiji@gmail.com>
2021-08-01 19:15:14 +0900

    投稿、投稿一覧機能

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,11 +1,23 @@
 class IdeasController < ApplicationController
   def index
+    @ideas = Idea.includes(:user).order(id: 'DESC')
   end
 
   def new
+    @idea = Idea.new
   end
 
   def create
+    @idea = Idea.new(idea_params)
+    if @idea.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+  def idea_params
+    params.require(:idea).permit(:catchphrase, :detail, :category_id, :image).merge(user_id: current_user.id)
+  end
 end
