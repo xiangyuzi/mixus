e2c3bb55f405aa9895d92c2515eb3a76cbdc577d
xiangyuzi <ayuko.weiji@gmail.com>
2021-08-03 18:35:00 +0900

    much-up機能、creationモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'ideas#index'
-  resources :ideas, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :ideas do
+    resources :orders, only: [:index, :create]
+  end
   resources :users, only: [:show]
 end
--- db/migrate/20210802083605_create_orders.rb
+++ db/migrate/20210802083605_create_orders.rb
@@ -1,10 +1,10 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.integer :deadline_id, null: false
-      t.string :email, null: false
-      t.references :user, null: false, foreign_key: true
-      t.references :idea, null: false, foreign_key: true
+      t.integer    :deadline_id, null: false
+      t.string     :email,       null: false
+      t.references :user,        null: false, foreign_key: true
+      t.references :idea,        null: false, foreign_key: true
       t.timestamps
     end
   end
