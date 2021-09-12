9b63d3dba72be57a6c3bbc2b5fe6e3fbc6c27a74
xiangyuzi <ayuko.weiji@gmail.com>
2021-09-12 23:26:09 +0900

    jQuery, slick導入

---
+++ app/javascript/idea.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.slider').slick({
+  dots: true,
+  autoplay: true,
+  autoplyaSpeed: 5000,
+  pauseOnHover: false,
+  arrows: true,
+  });
+});
+
+
+$(function() {
+  console.log("OK");
+});
