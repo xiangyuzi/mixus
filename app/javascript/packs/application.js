fa56faf050159fc949caa7130d79de0e749df15d
xiangyuzi <ayuko.weiji@gmail.com>
2021-09-19 21:34:44 +0900

    chartkick導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("../idea")
-
+require("chartkick")
+require("chart.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
